57205a78ab9caade696f6947ba8bfa06c4c9a3d9
Gerardo Duron <gerardo.duron-pena@my.tccd.edu>
2020-04-08 04:46:16 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <string>
+#include<conio.h>
+using namespace std;
+
+int main()
+
+{
+  string name; 
+  cout << "What is your name? ";
+  cin >> name;
+  cout << "Hi " << name << "\n";
+  
+  
+	int arr[130], size, insert, i, pos;
+	cout<<"Please Enter Array Size : ";
+	cin>>size;
+	cout<<"Enter Array elements : ";
+	for(i=0; i<size; i++)
+	{
+		cin>>arr[i];
+  }
+}
